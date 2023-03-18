cde4d71ff0f14e6433b61ec1fd023c761a31bbf0
SolomonOdunusi <solomonodunusi@gmail.com>
2023-03-18 05:56:14 -0700

    New commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Print all digit combination
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+        int i, j, n;
+
+        for(i = 0; i <= 9; i++)
+
+        {
+                for(j = i + 1; j <= 9; j++)
+                {
+			for(n = j + 1; n <= 9; n++)
+			{
+                        putchar('0' + i);
+                        putchar('0' + j);
+			putchar('0' + n);
+                        if(i != 7 || j != 8 || n != 9)
+                        {
+                                putchar(',');
+                                putchar(' ');
+                        }
+			}
+                }
+        }
+        return (0);
+}
---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Description: Print all digit combination
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i, j, a, b, c, d;
+
+	for(i = 0; i <= 99; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+		for(j = i; j <= 99; j++)
+		{
+			c = j / 10;
+			d = j % 10;
+			putchar('0' + a);
+			putchar('0' + b);
+			putchar(' ');
+			putchar('0' + c);
+			putchar('0' + d);
+			if(i != 99 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
