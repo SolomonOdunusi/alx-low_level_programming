d3ff3b5e3d26a3b6876e2741cf5f5f0a53620386
SolomonOdunusi <solomonodunusi@gmail.com>
2023-03-23 10:55:55 -0700

    Fixed return type error

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -11,9 +11,10 @@
 int positive_or_negative(int i)
 {
 	if (i > 0)
-		printf("%d is positive\n");
+		printf("%d is positive\n", i);
 	else if (i < 0)
 		printf("%d is negative\n", i);
 	else
 		printf("%d is zero\n", i);
+	return (0);
 }
