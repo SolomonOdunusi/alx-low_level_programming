5393ebafff5673eec2cdfce8f52d69d61240b316
SolomonOdunusi <solomonodunusi@gmail.com>
2023-03-23 10:43:18 -0700

    First commit on nested loops

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,9 +1,17 @@
-#include "main.h"
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
 
-void positive_or_negative(int i)
+/**
+* positive_or_negative - assign a random num to the var n each time
+* @i : integer
+* Return: Always 0 (success)
+*/
+
+int positive_or_negative(int i)
 {
 	if (i > 0)
-		printf("%d is positive\n", i);
+		printf("%d is positive\n");
 	else if (i < 0)
 		printf("%d is negative\n", i);
 	else
