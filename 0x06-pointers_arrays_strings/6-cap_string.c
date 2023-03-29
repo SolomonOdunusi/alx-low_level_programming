ff7799456bc30491cb991babfc97e3057e545ac1
SolomonOdunusi <solomonodunusi@gmail.com>
2023-03-29 16:35:47 -0700

    New commit

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,8 +12,9 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
+		{
 			index++;
-
+		}
 		if (str[index - 1] == ' ' ||
 		    str[index - 1] == '\t' ||
 		    str[index - 1] == '\n' ||
@@ -28,8 +29,9 @@ char *cap_string(char *str)
 		    str[index - 1] == '{' ||
 		    str[index - 1] == '}' ||
 		    index == 0)
+		{
 			str[index] -= 32;
-
+		}
 		index++;
 	}
 
