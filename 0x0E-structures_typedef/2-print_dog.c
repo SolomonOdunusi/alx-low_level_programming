17e3fbcecfd6bc89a7e3ec8ae4d36630949d2d23
SolomonOdunusi <solomonodunusi@gmail.com>
2023-04-18 09:37:30 -0700

    2- Made structured function and defined new types

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,6 +14,6 @@ void print_dog(struct dog *d)
 		return;
 	}
 	printf("Name: %s\n", PRINT_OR_NIL(d->name));
-	printf("Name: %f\n", d->age);
+	printf("Name: %.6f\n", d->age);
 	printf("Name: %s\n", PRINT_OR_NIL(d->owner));
 }
