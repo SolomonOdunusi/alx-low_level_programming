fc3cf7350871103b0499e689c54ee4d9e939f74e
SolomonOdunusi <solomonodunusi@gmail.com>
2023-05-08 15:32:29 -0700

    first commit

---
+++ 0x15-file_io/0-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ * ac: arg count
+ * av: arg vector
+ * Return: Always 0.
+ */
+int main(int ac, char **av)
+{
+	ssize_t n;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "Usage: %s filename\n", av[0]);
+		exit(1);
+	}
+	n = read_textfile(av[1], 114);
+	printf("(printed chars: %li)\n", n);
+	n = read_textfile(av[1], 1024);
+	printf("(printed chars: %li)\n", n);
+	return (0);
+}
---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints to POSIX standard output
+ * @filename: the name of the file
+ * @letters: letters to count
+ * Return: returns the text in file plus number of chars
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	FILE *fp = fopen(filename, "r");
+
+	if (fp == NULL)
+	{
+		return (0);
+	}
+
+	char *buffer = (char*) malloc(sizeof(char) * letters);
+
+	if (buffer == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+
+	ssize_t num_read = fread(buffer, sizeof(char), letters, fp);
+
+	if (num_read == -1)
+	{
+		fclose(fp);
+		free(buffer);
+		return (0);
+	}
+
+	ssize_t num_written = write(STDOUT_FILENO, buffer, num_read);
+	
+	if (num_written == -1 || num_written != num_read)
+	{
+		fclose(fp);
+		free(buffer);
+		return (0);
+	}
+
+	fclose(fp);
+	free(buffer);
+
