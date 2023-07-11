8ddb3a6b4e65d24de2031cb1c72a78f1a870ba7f
Solomon Odunusi <solomonodunsui@gmail.com>
2023-07-11 09:27:36 -0700

    Fixed error

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,17 +8,22 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, i;
+	int fd;
+	int i;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
+
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, FILE_PERM);
+
 	if (fd == -1)
 	{
 		return (-1);
 	}
+
+	ftruncate(fd, 0)
 	for (i = 0; text_content[i]; i++)
 	{
 		write(fd, &text_content[i], 1);
