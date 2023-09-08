7a0583a35504dcfc4f9392b9e1cc647a3ea6c58f
Solomon Odunusi <solomonodunsui@gmail.com>
2023-09-07 23:35:46 -0700

    Debugged the hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,6 +12,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_node_t **array;
 	unsigned long int i;
 
+	if (size == 0)
+		return (NULL);
 	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 		return (NULL);
